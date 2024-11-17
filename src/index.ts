6b7756a82970efd433e22896339abdf20815b494
sousuke-code <msousuke1@gmail.com>
2024-11-17 23:13:03 +0900

    基本設定

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import express from "express";
+
+import dotenv from "dotenv";
+import { Client, WebhookEvent,middleware } from "@line/bot-sdk";
+import ngrok  from "ngrok";
+
+dotenv.config();
+
+
+const app = express();
+const port = parseInt(process.env.PORT || "3000", 10);
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN!,
+  channelSecret: process.env.CHANNEL_SECRET!,
+};
+
+app.use(middleware(config));
+
+const client = new Client(config);
+
+
+
+app.post("/webhook", async(req, res) => {
+    try {
+        const events = req.body.events;
+        await Promise.all(events.map(handleEvent));
+        res.status(200).send("OK")
+    } catch (error) {
+        console.log("faile to connect");
+        res.status(500).send("internal error");
+    }
+})
+
+function handleEvent(event: WebhookEvent) {
+    if(event.type !== "message" || event.message.type !== "text") {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, [
+        {
+            type: "text",
+            text: event.message.text,
+        }
+    ])
+}
+
+app.listen(port, async() => {
+    console.log(`Sercver is runnning on port ${port}`);
+
+    try {
+        const url = await ngrok.connect(port); 
+        console.log(`ngrok URL: ${url}/webhook`);     
+    } catch (error) {
+        
+    }
+})
