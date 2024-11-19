c5a32e6bc3d1b135650f922bc31987bc3e74c9c4
sousuke-code <msousuke1@gmail.com>
2024-11-19 21:12:03 +0900

    supabaseへのデータ挿入

--- src/googleAuth.ts
+++ src/googleAuth.ts
@@ -1,26 +1,55 @@
 import { google } from "googleapis";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { supabase } from "./Supabase";
 
 dotenv.config();
 
+const app = express();
+
 export const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID, // Google Cloudで取得したクライアントID
-    process.env.GOOGLE_CLIENT_SECRET, // クライアントシークレット
-    process.env.REDIRECT_URI // リダイレクトURI
+  process.env.GOOGLE_CLIENT_ID, 
+  process.env.GOOGLE_CLIENT_SECRET, 
+  process.env.REDIRECT_URI 
 );
 
 export const generateAuthUrl = () => {
-    const scopes = ["https://www.googleapis.com/auth/calendar"];
-    return oauth2Client.generateAuthUrl({
-        access_type: "offline",
-        scope: scopes,
-    })
-}
+  const scopes = ["https://www.googleapis.com/auth/calendar"];
+  return oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+};
 
-export const getAccessToken = async (code :string) => {
-    const { tokens } =await oauth2Client.getToken(code);
-    oauth2Client.setCredentials(tokens);
-    return tokens;
-}
+export const getAccessToken = async (code: string) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
+  return tokens;
+};
 
+app.get(
+  "/oauth2callback",
+  async (req, res)  => {
+    const code = req.query.code as string;
+    const userId = req.query.state as string;
 
+    if (!code) {
+      return res.status(400);
+    }
+
+    try {
+      const tokens = await getAccessToken(code);
+      oauth2Client.setCredentials(tokens);
+
+      const { error }  = await supabase.from('user_tokens').insert({ access_token: tokens.access_token, line_user_id: userId});
+
+      if (error) {
+          throw new Error("failed to insert information to DB");
+    }
+
+      res.status(200);
+    } catch (error) {
+      res.status(500);
+    }
+  }
+);
---
+++ src/supabase.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+
+if (!supabaseUrl || !supabaseAnonKey){
+    throw new Error("not found supabase enviroment variable");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
