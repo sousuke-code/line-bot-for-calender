0b74c81df21ba9a474ca8aaadf8e593f2b989b07
sousuke-code <msousuke1@gmail.com>
2024-11-19 23:25:17 +0900

    最左プロイ

--- src/googleAuth.ts
+++ src/googleAuth.ts
@@ -10,7 +10,7 @@ const app = express();
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID, 
   process.env.GOOGLE_CLIENT_SECRET, 
-  process.env.REDIRECT_URI 
+ "https://line-bot-for-calender.vercel.app/oauth2callback"
 );
 
 export const generateAuthUrl = () => {
