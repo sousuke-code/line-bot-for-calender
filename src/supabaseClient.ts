1098055c0f81014f411b93809e28bbfb20669ae2
sousuke-code <msousuke1@gmail.com>
2024-11-19 21:32:48 +0900

    環境変数の再設定

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 
+dotenv.config();
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
