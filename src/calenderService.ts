5d333dadb98ef745a61b929ab42b849de3131394
sousuke-code <msousuke1@gmail.com>
2024-11-28 21:38:05 +0900

    セッション情報の変更

--- src/calenderService.ts
+++ src/calenderService.ts
@@ -1,13 +1,20 @@
 import {google} from "googleapis";
 import { oauth2Client } from "./googleAuth";
 import { getUserAccessToken } from "./googleAuth";
+import { supabase } from "./supabaseClient";
 
 
 
 export const addEventToCalendar = async(userId: string,event: any) => {
-    const tokens = await getUserAccessToken(userId);
+    const { data: {session}, error: sessionError} = await supabase.auth.getSession();
+    if (!session || sessionError) {
+        console.error("Failed to retrieve user session:", sessionError || "No session found");
+        throw new Error("User not authenticated or session not found");
+    }
 
-    oauth2Client.setCredentials(tokens);
+    const accessToken = session.provider_token;
+
+    oauth2Client.setCredentials({ access_token: accessToken});
 
     const calendar = google.calendar({ version: "v3", auth: oauth2Client});
 
